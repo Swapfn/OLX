8cb4ace8b73f5a24f01edea196a1f8ed9c168465
MariamBeltagy <maeraim2@gmail.com>
2022-07-06 12:41:26 +0200

    models init

---
+++ Backend/Models/Models/Location.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models.Models
+{
+    public class Location
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int LocationId { set; get; }
+        public string City { set; get; } //change??
+        public List<User> Users { set; get; }
+        public Location()
+        {
+            Users = new List<User>();
+        }
+    }
+}
---
+++ Backend/Models/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { set; get; }
+        public string FName { set; get; }
+        public string LName { set; get; }
+        public int Phone { set; get; }
+        public string Email { set; get; }
+        [PasswordPropertyText(true)]
+        public string Password { set; get; }
+        [ForeignKey("Role")]
+        public int RoleId { set; get; }
+        public Role Role { get; set; }
+        [ForeignKey("Location")]
+        public Location LocationId { set; get; }
+        public Location Location { get; set; }
+    }
+}
