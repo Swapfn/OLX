a6eb8af01a88a72252179e4bf275573872e6bb42
Noha Saab <noha.sa3b@gmail.com>
2022-07-14 14:40:57 +0200

    added frontpage

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LocationFilterComponent } from './location-filter/location-filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomePageComponent,
+    LocationFilterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Frontend/src/app/nav/nav.component.ts
@@ -0,0 +1,48 @@
+import { outputAst } from '@angular/compiler';
+import { Component, OnInit ,Input,EventEmitter, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  //______________________Location Filter_________________________________________
+  EgyCities=["Alexandria","Aswan","Asyut","Beheira","Beni Suef","Cairo","Dakahlia","Damietta",
+  "Faiyum","Gharbia","Giza","Ismailia","Kafr El Sheikh","Luxor","Matruh","Minya","Monufia","New Valley",
+  "North Sinai","Port Said","Qalyubia","Qena","Red Sea","Sharqia","Sohag","South Sinai","Suez"]
+
+  locationSelectedValue:string="Egypt";
+
+  @Output()
+  locationselect:EventEmitter<string> = new EventEmitter<string>();
+  
+  onlocationSelect(e:any){
+    this.locationselect.emit(this.locationSelectedValue)
+    //console.log(this.locationSelectedValue);
+  }
+
+
+  //________________________________Search___________________________________________
+
+  searchValue:string="";
+  
+  @Output()
+  searchText:EventEmitter<string>= new EventEmitter<string>();
+
+  OnsearchText(){
+    this.searchText.emit(this.searchValue);
+    
+  }
+
+
+
+
+
+  
+}
