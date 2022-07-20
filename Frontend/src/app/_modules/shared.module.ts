888edc5ad5ef191f5e46e4daede074a6d4ca0c81
MoAyman98 <mohamed.elhabet98@gmail.com>
2022-07-20 18:25:47 +0200

    Shared module, guard, toastr

---
+++ Frontend/src/app/_modules/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { ToastrModule } from 'ngx-toastr';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BsDropdownModule.forRoot(),
+    ModalModule.forRoot(),
+    ToastrModule.forRoot({
+      positionClass: "toast-bottom-right"
+    })
+  ],
+  exports: [
+    CommonModule,
+    BsDropdownModule,
+    ModalModule,
+    ToastrModule
+  ]
+})
+export class SharedModule { }
