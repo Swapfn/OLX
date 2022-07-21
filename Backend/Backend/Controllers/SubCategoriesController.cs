6f0ae7790a4da60c38644d225ff740801ad93fb7
Safwa Salem <safwa.salem@gmail.com>
2022-07-21 17:11:00 +0200

    refactor subcategories

---
+++ Backend/Backend/Controllers/SubCategoriesController.cs
@@ -0,0 +1,112 @@
+﻿using Data.Repositories.Contracts;
+using Mapper.Contracts;
+using Mapper.Mappers;
+using Microsoft.AspNetCore.Mvc;
+using Models.DTO;
+using Models.Models;
+using Services;
+
+namespace WepAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubCategoriesController : BaseController
+    {
+        private readonly ISubCategoryService _subCategoryService;
+        private readonly ISubCategoryRepository _subCategoryRepository;
+
+        public SubCategoriesController(ISubCategoryService subCategoryService, ISubCategoryRepository subCategoryRepository)
+        {
+            _subCategoryService = subCategoryService;
+            _subCategoryRepository = subCategoryRepository;
+        }
+
+        // GET api/SubCategories/getByCategoryId/1
+        [HttpGet]
+        [Route("getByCategoryId/{id}")]
+        public IActionResult GetAllByCategoryId(int id)
+        {
+            IEnumerable<SubCategoryDTO> result = _subCategoryService.GetAllByCategoryId(id);
+            return Ok(result);
+        }
+
+        // GET api/SubCategories/1
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById(int id)
+        {
+            if (!SubCategoryExists(id))
+            {
+                return NotFound();
+            }
+
+            SubCategoryDTO subCategoryDTO = _subCategoryService.GetById(id);
+            return Ok(subCategoryDTO);
+
+        }
+
+        // POST api/Subcategories
+        [HttpPost]
+        [Route("")]
+        public IActionResult Add(SubCategoryDTO subCategoryDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            SubCategoryDTO result = _subCategoryService.Add(subCategoryDTO);
+            _subCategoryService.SaveSubCategory();
+            return Ok(result);
+        }
+
+
+        // PUT api/Subcategories/1
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Update(int id, SubCategoryDTO subCategoryDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != subCategoryDTO.SubCategoryID)
+            {
+                return BadRequest();
+            }
+
+            if (!SubCategoryExists(id))
+            {
+                return NotFound();
+            }
+
+            _subCategoryService.Update(id, subCategoryDTO);
+            _subCategoryService.SaveSubCategory();
+            SubCategoryDTO result = _subCategoryService.GetById(id);
+
+            return Ok(result);
+        }
+
+        // DELETE api/subcategories/1
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult DeleteSubCategory(int id)
+        {
+            if (!SubCategoryExists(id))
+            {
+                return NotFound();
+            }
+
+            _subCategoryService.Delete(id);
+            _subCategoryService.SaveSubCategory();
+            return Ok("SubCategory deleted");
+
+        }
+        private bool SubCategoryExists(int id)
+        {
+            SubCategory subCategory = _subCategoryRepository.GetById(id);
+            return subCategory != null;
+        }
+    }
+}
