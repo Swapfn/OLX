9d763675e79fb0ff856380cdd969259d631183a5
Safwa Salem <safwa.salem@gmail.com>
2022-07-21 11:17:24 +0200

    add locations crud

---
+++ Backend/Backend/Controllers/LocationController.cs
@@ -0,0 +1,64 @@
+﻿using Mapper.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models.DTO;
+using Services;
+
+namespace WepAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        private readonly ILocationService _locationService;
+        private readonly ILocationMapper _locationMapper;
+        public LocationController(ILocationService locationService, ILocationMapper locationMapper)
+        {
+            _locationService = locationService;
+            _locationMapper = locationMapper;
+        }
+
+        [HttpGet]
+        [Route("locations")]
+        public IActionResult GetAll()
+        {
+            IEnumerable<LocationDTO> locationsDTO = _locationService.GetAll();
+            return Ok(locationsDTO);
+        }
+
+        [HttpGet]
+        [Route("locations/{id}")]
+        public IActionResult GetById(int id)
+        {
+            LocationDTO locationDTO = _locationService.GetById(id);
+            return Ok(locationDTO);
+        }
+
+        [HttpPost]
+        [Route("locations")]
+        public IActionResult Add(LocationDTO locationDTO)
+        {
+            _locationService.Add(locationDTO);
+            _locationService.SaveLocation();
+            return Ok(locationDTO);
+        }
+
+        [HttpPut]
+        [Route("locations/{id}")]
+        public IActionResult Update(int id, LocationDTO locationDTO)
+        {
+            _locationService.Update(id, locationDTO);
+            _locationService.SaveLocation();
+            return Ok(locationDTO);
+        }
+
+        [HttpDelete]
+        [Route("locations/{id}")]
+        public IActionResult Delete(int id)
+        {
+            _locationService.Delete(id);
+            _locationService.SaveLocation();
+            return Ok();
+        }
+    }
+}
