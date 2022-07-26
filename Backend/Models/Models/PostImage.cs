a9f03edd46b185c65021aeca45841b01b8e569cd
Safwa Salem <safwa.salem@gmail.com>
2022-07-26 20:26:04 +0200

    refactoring

--- Backend/Models/Models/PostImage.cs
+++ Backend/Models/Models/PostImage.cs
@@ -5,12 +5,8 @@ namespace Models.Models
 {
     public class PostImage
     {
-        //[Key]
-        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int PostImageID { get; set; }
-        //public byte[] ImageContent { get; set; }
         public string ImageURL { get; set; }
-        //[ForeignKey("Post")]
         public int PostId { set; get; }
         public Post Post { get; set; }
 
