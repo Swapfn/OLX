30bd84b25853c1835c0eeeff79fdd7998959e586
Noha Saab <noha.sa3b@gmail.com>
2022-07-26 19:33:25 +0200

    Merge error fix

--- Frontend/src/app/_models/token.ts
+++ Frontend/src/app/_models/token.ts
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-export interface Token{
-    message:string;
-    token:string;
-    expiration:string;
-=======
 export interface Token {
     message: string;
     token: string;
     expiration: string;
->>>>>>> origin/Add-Post
 }
