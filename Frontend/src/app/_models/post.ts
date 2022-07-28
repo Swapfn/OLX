fbe28d86cf5ae2c3ad5a36dbbe40c6576b257fe9
Noha Saab <noha.sa3b@gmail.com>
2022-07-28 15:33:22 +0200

    post details Done

---
+++ Frontend/src/app/_models/post.ts
@@ -0,0 +1,23 @@
+
+export interface Post{
+    
+postId:number;
+title:string;
+description:string;
+createdAt:string;
+price:number;
+isNew:boolean;
+isNegotiable:boolean;
+isAvailable:boolean;
+categoryId:number;
+subCategoryId:number;
+locationId:number;
+userID:number;
+subCategoryName:string;
+cityName:string;
+fullName:string;
+phoneNumber:number;
+minPrice:number;
+maxPrice:number;
+
+}
