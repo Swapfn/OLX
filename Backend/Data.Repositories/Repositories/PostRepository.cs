b7305e64d7dbcb8e01dedbfa13c4ff68d9c67998
Safwa Salem <safwa.salem@gmail.com>
2022-08-01 18:11:05 +0200

    Revert "Revert "get available posts only""
    This reverts commit 5549784a5bb4c1f62e95fd1adc6cb6d9ba18c95b.

--- Backend/Data.Repositories/Repositories/PostRepository.cs
+++ Backend/Data.Repositories/Repositories/PostRepository.cs
@@ -91,8 +91,79 @@ namespace Data.Repositories.Repositories
                 }
             }
 
-            posts.TotalRecords = this.DbContext.Posts.Count();
+            posts.TotalRecords = posts.Results.Count();
             return posts;
+        }
+
+        public PagedResult<Post> GetAll(FilterDTO<PostDTO> FilterObject)
+        {
+            PagedResult<Post> posts = new PagedResult<Post>();
+            Expression<Func<Post, bool>> SearchCriteria = a => (
+            (a.Title.Contains(FilterObject.SearchObject.Title) || string.IsNullOrEmpty(FilterObject.SearchObject.Title))
+            &&
+            (a.Description.Contains(FilterObject.SearchObject.Description) || string.IsNullOrEmpty(FilterObject.SearchObject.Description))
+            &&
+            (a.SubCategoryId == FilterObject.SearchObject.SubCategoryId || FilterObject.SearchObject.SubCategoryId == 0)
+            &&
+            (a.LocationId == FilterObject.SearchObject.LocationId || FilterObject.SearchObject.LocationId == 0)
+            &&
+            (a.SubCategory.CategoryID == FilterObject.SearchObject.CategoryId || FilterObject.SearchObject.CategoryId == 0)
+            &&
+            (a.Price >= FilterObject.SearchObject.minPrice || FilterObject.SearchObject.minPrice == 0)
+            &&
+            (a.Price <= FilterObject.SearchObject.maxPrice || FilterObject.SearchObject.maxPrice == 0)
+            );
+            posts = this.GetAll(FilterObject.PageNumber, FilterObject.PageSize, FilterObject.Includes, SearchCriteria, FilterObject.SortBy, FilterObject.SortDirection);
+            return posts;
+        }
+
+        private PagedResult<Post> GetAll(
+            int PageNumber,
+            int PageSize,
+            List<string> includes,
+            Expression<Func<Post, bool>> filter = null,
+            string SortBy = "",
+            string SortDirection = ""
+            )
+        {
+            PagedResult<Post> PagedList = new PagedResult<Post>();
+            IQueryable<Post> Query = this.DbContext.Posts.AsQueryable<Post>();
+            foreach (string include in includes)
+            {
+                Query = Query.Include(include).Where(p => p.IsAvailable);
+            }
+            string SortByParam = "CreationDate";
+            string SortDirectionParam = "ASC";
+
+            if (!string.IsNullOrEmpty(SortBy))
+            {
+                SortByParam = SortBy;
+            }
+            if (!string.IsNullOrEmpty(SortDirection))
+            {
+                SortDirectionParam = SortDirection;
+            }
+            if (filter != null)
+            {
+                Query = Query.Where(filter);
+            }
+
+            PagedList.TotalRecords = Query.AsNoTracking().ToList().Count();
+
+            if (SortDirectionParam.ToLower() == "asc")
+            {
+                Query = Query.OrderBy(SortByParam);
+            }
+            else
+            {
+                Query = Query.OrderByDescending(SortByParam);
+            }
+
+
+            Query = Query.Skip((PageNumber - 1) * PageSize);
+
+            PagedList.Results = Query.Take(PageSize).AsNoTracking().ToList();
+            return PagedList;
         }
 
         public bool IsExist(int id)
