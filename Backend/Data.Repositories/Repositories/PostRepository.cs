98b0cb0a8671ff6fcb9ae04bfda7f53cccc0bee8
Safwa Salem <safwa.salem@gmail.com>
2022-08-04 00:09:42 +0200

    get user posts "pagination"

--- Backend/Data.Repositories/Repositories/PostRepository.cs
+++ Backend/Data.Repositories/Repositories/PostRepository.cs
@@ -96,15 +96,17 @@ namespace Data.Repositories.Repositories
             &&
             (a.Description.Contains(FilterObject.SearchObject.Description) || string.IsNullOrEmpty(FilterObject.SearchObject.Description))
             &&
-            (a.SubCategoryId == FilterObject.SearchObject.SubCategoryId || FilterObject.SearchObject.SubCategoryId == 0|| FilterObject.SearchObject.SubCategoryId == null)
+            (a.UserID == FilterObject.SearchObject.UserID || FilterObject.SearchObject.UserID == 0 || FilterObject.SearchObject.UserID == null)
+            &&
+            (a.SubCategoryId == FilterObject.SearchObject.SubCategoryId || FilterObject.SearchObject.SubCategoryId == 0 || FilterObject.SearchObject.SubCategoryId == null)
             &&
             (a.LocationId == FilterObject.SearchObject.LocationId || FilterObject.SearchObject.LocationId == 0 || FilterObject.SearchObject.LocationId == null)
             &&
             (a.SubCategory.CategoryID == FilterObject.SearchObject.CategoryId || FilterObject.SearchObject.CategoryId == 0 || FilterObject.SearchObject.CategoryId == null)
             &&
-            (a.Price >= FilterObject.SearchObject.minPrice || FilterObject.SearchObject.minPrice == 0|| FilterObject.SearchObject.minPrice == null)
+            (a.Price >= FilterObject.SearchObject.minPrice || FilterObject.SearchObject.minPrice == 0 || FilterObject.SearchObject.minPrice == null)
             &&
-            (a.Price <= FilterObject.SearchObject.maxPrice || FilterObject.SearchObject.maxPrice == 0|| FilterObject.SearchObject.maxPrice == null)
+            (a.Price <= FilterObject.SearchObject.maxPrice || FilterObject.SearchObject.maxPrice == 0 || FilterObject.SearchObject.maxPrice == null)
             );
             posts = this.GetAll(FilterObject.PageNumber, FilterObject.PageSize, FilterObject.Includes, SearchCriteria, FilterObject.SortBy, FilterObject.SortDirection);
             return posts;
@@ -125,7 +127,7 @@ namespace Data.Repositories.Repositories
             {
                 Query = Query.Include(include).Where(p => p.IsAvailable);
             }
-            string SortByParam = "CreationDate";
+            string SortByParam = "CreatedAt";
             string SortDirectionParam = "ASC";
 
             if (!string.IsNullOrEmpty(SortBy))
